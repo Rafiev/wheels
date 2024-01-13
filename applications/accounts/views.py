3c851fb528fcddd9c50d366346ad9f518735703e
Rafiev <rafievvvv@gmail.com>
2024-01-13 16:36:41 +0600

    added register

--- applications/accounts/views.py
+++ applications/accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import views, status
+from django.contrib.auth import get_user_model
+from applications.accounts.serializers import RegisterSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+
+User = get_user_model()
+
+
+class RegisterApiView(views.APIView):
+
+    @staticmethod
+    def post(request):
+        serializer = RegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response('You have successfully registered', status=status.HTTP_201_CREATED)
