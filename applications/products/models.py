79c4ad34a7e4d9aa3df7c7eca4c6a523c8b4c561
Rafiev <rafievvvv@gmail.com>
2024-01-16 20:16:41 +0600

    addede crud Wheel, added team for users

--- applications/products/models.py
+++ applications/products/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from applications.accounts.models import Team
 
 User = get_user_model()
 
 
 class Storage(models.Model):
+    owner = models.ForeignKey(Team, on_delete=models.CASCADE, null=True, related_name='storages')
     title = models.CharField(max_length=30)
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
 
@@ -13,8 +15,10 @@ class Storage(models.Model):
 
 
 class Wheel(models.Model):
+    owner = models.ForeignKey(Team, on_delete=models.CASCADE, null=True, related_name='wheels')
     title = models.CharField(max_length=50)
     amount = models.PositiveIntegerField(default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     storage = models.ForeignKey(Storage, on_delete=models.CASCADE, related_name='wheels')
 
     def __str__(self):
